00a6e9b0fc805d95376a29171d73783f8c682724
ANDRES FELIPE MEJIA CORRALES <andresmejia246388@correo.itm.edu.co>
2025-09-15 00:21:05 -0500

    ADD

--- Employee/Orders.Backend/Program.cs
+++ Employee/Orders.Backend/Program.cs
@@ -1,14 +1,38 @@
 using Employee.Backend.Data;
-using Microsoft.Extensions.DependencyInjection;
+using Employee.Backend.Repositories.Implementations;
+using Employee.Backend.Repositories.Interfaces;
+using Employee.Backend.UnitsOfWork.Implementations;
+using Employee.Backend.UnitsOfWork.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("Name=LocalConnection"));
+builder.Services.AddTransient<SeedDb>();
 
+builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+builder.Services.AddScoped<IEmployeesUnitOfWork, EmployeesUnitOfWork>();
+builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
+
+// Inyecciones antes del Build()
 var app = builder.Build();
+
+seedData(app);
+
+void seedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using var scope = scopedFactory!.CreateScope();
+    var service = scope.ServiceProvider.GetService<SeedDb>();
+    service!.SeedAsync().Wait(); // Wait to call async from sync program
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,9 +40,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
