0f152f4a577c8243df4881bed4ebf545dc5c9a9e
ANDRES FELIPE MEJIA CORRALES <andresmejia246388@correo.itm.edu.co>
2025-09-14 19:25:42 -0500

    Add PUT and GET for employees

---
+++ Employee/Orders.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using Employee.Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public EmployeesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Employee.Shared.Entities.Employee employee)
+    {
+        _context.Employess.Add(employee);
+        await _context.SaveChangesAsync();
+        return Ok(employee);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Employess.ToListAsync());
+    }
+}
